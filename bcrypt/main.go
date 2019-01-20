b428e500a5a076d06231e637683c3289cfb5b9c1
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-01-21 01:23:49 +0900

    add brypt

---
+++ bcrypt/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"os"
+)
+
+func main() {
+	password := "password"
+	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("bcrypted:", string(bs))
+
+	if err := bcrypt.CompareHashAndPassword(bs, []byte(password)); err != nil {
+		fmt.Println("invalid password")
+		os.Exit(1)
+	}
+	fmt.Println("matched", string(bs), "and", password)
+
+	cost, err := bcrypt.Cost(bs)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("cost", cost)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module github.com/cappyzawa/golang-lab
 
-require golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4
+require (
+	golang.org/x/crypto v0.0.0-20190103213133-ff983b9c42bc // indirect
+	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4
+)
