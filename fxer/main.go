70f95a6096bc8a334ff9812a29d37a66bc402ec9
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-08-21 23:56:03 +0900

    add fxer

---
+++ fxer/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"go.uber.org/fx"
+)
+
+func NewLogger() *log.Logger {
+	logger := log.New(os.Stdout, "" /* prefix */, 0 /* flags */)
+	logger.Print("Executing NewLogger.")
+	return logger
+}
+
+func NewHandler(logger *log.Logger) (http.Handler, error) {
+	logger.Print("Executing NewHandler.")
+	return http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
+		logger.Print("Got a request.")
+	}), nil
+}
+
+func Register(mux *http.ServeMux, h http.Handler) {
+	mux.Handle("/", h)
+}
+
+func NewMux(lc fx.Lifecycle, logger *log.Logger) *http.ServeMux {
+	logger.Print("Executing NewMux.")
+	// First, we construct the mux and server. We don't want to start the server
+	// until all handlers are registered.
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	lc.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			logger.Print("Starting HTTP server.")
+			go server.ListenAndServe()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			logger.Print("Stopping HTTP server.")
+			return server.Shutdown(ctx)
+		},
+	})
+
+	return mux
+}
+
+func main() {
+	app := fx.New(
+		// Provide all the constructors we need, which teaches Fx how we'd like to
+		// construct the *log.Logger, http.Handler, and *http.ServeMux types.
+		// Remember that constructors are called lazily, so this block doesn't do
+		// much on its own.
+		fx.Provide(
+			NewLogger,
+			NewHandler,
+			NewMux,
+		),
+		// Since constructors are called lazily, we need some invocations to
+		// kick-start our application. In this case, we'll use Register. Since it
+		// depends on an http.Handler and *http.ServeMux, calling it requires Fx
+		// to build those types using the constructors above. Since we call
+		// NewMux, we also register Lifecycle hooks to start and stop an HTTP
+		// server.
+		fx.Invoke(Register),
+	)
+
+	startCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	if err := app.Start(startCtx); err != nil {
+		log.Fatal(err)
+	}
+
+	http.Get("http://localhost:8080/")
+
+	stopCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	if err := app.Stop(stopCtx); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ require (
 	github.com/sergi/go-diff v1.0.0
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.4.0
+	go.uber.org/dig v1.7.0 // indirect
+	go.uber.org/fx v1.9.0
 	golang.org/x/crypto v0.0.0-20190621222207-cc06ce4a13d4
 	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4
 	gopkg.in/yaml.v2 v2.2.2
